5c9acbcdbf532e8e1e2a348f69a879aa71ebee93
vaishnavi sisodiya <vaishvi.sisodiya28@gmail.com>
2024-06-09 14:47:10 +0530

    commited

--- src/component/ProjectCardDetail.jsx
+++ src/component/ProjectCardDetail.jsx
@@ -2,19 +2,19 @@ const ProjectCardDetail = [
     {
         imgsrc:'./img/project1.png',
         title:'Portfolio',
-        text:'I created a responsive personal portfolio website using React. Create multiple function-based components like Contact, Navbar, About, Footer, Project, Skills, etc., and at the end, integrat all files into App. js. Styling and design are implemented through React Bootstrap.Showcases projects with interactive UI components Utilizes state management with React hooks.',
+        text:'I created a responsive personal portfolio website using React. Create multiple function-based components like Contact, Navbar, About, Footer, Project, Skills, etc., and at the end, integrat all files into App. js.',
         view:"https://youtu.be/0h2b4ftbZcU?si=H3cqdo-OJopOTSfk"
     },
     {
-        imgsrc:'./img/tic-tac-toe.jpg',
+        imgsrc:'./img/banner_tic.jpg',
         title:'Tic-Tac-Toe',
-        text:' I created a Tic-Tac-Toe Game using JavaScript. I have used validation checks to implement the game features. The UI is designed using CSS so it is easy to create. In the game, Player-1 starts playing the game and both players make their moves in consecutive turns.',
+        text:' I created a Tic-Tac-Toe Game using JavaScript. I have used validation checks to implement the game features. The UI is designed using CSS so it is easy to create.',
         view:"https://www.linkedin.com/posts/avishek-patra-ap_scamalert-fresherbeware-internshala-activity-7079371682880049152-AoFw/"
     },
     {
-        imgsrc:'./img/wheather_img .png',
-        title:'Ecommerce web hgdjh,c',
-        text:'Cognifyz Technologies is rated 4.0 out of 5, based on 8 reviews by employees on AmbitionBox.  ',
+        imgsrc:'./img/wheather_image.jpg',
+        title:'Weather App',
+        text:' project built using Node.js : a weather app that provides current temperature information for any city around the world. This project has been a fantastic learning experience, and I would love to showcase some of its key features and technical details',
         view:"https://youtu.be/0h2b4ftbZcU?si=H3cqdo-OJopOTSfk"
     }
 ]
